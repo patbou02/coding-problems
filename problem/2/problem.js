052bed69c3f1712c7a919daba2e51c711336ecbf
Patrick Boutin <patrick.boutin@roberthalf.com>
2018-12-01 16:18:44 -0800

    Add starting problem solution.

--- problem/2/problem.js
+++ problem/2/problem.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let input = [1, 2, 3, 4, 5];
+let out = [];
+
+for (let i = 0; i < input.length; i++) {
+  let prod = 1;
+  for (let j = input.length - 1; j >= 0; j--) {
+    if (i !== j) {
+      prod = prod * input[j];
+    }
+  }
+  out[i] = prod;
+}
+console.log(out);
